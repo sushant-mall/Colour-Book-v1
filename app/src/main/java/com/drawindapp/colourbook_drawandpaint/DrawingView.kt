ea1b9cc91ed617f0c8359735d8f36b25fac04a5b
sushant-mall <52692251+sushant-mall@users.noreply.github.com>
2020-10-28 15:36:08 +0530

    Initial commit

--- app/src/main/java/com/drawindapp/colourbook_drawandpaint/DrawingView.kt
+++ app/src/main/java/com/drawindapp/colourbook_drawandpaint/DrawingView.kt
@@ -18,10 +18,18 @@ class DrawingView(context: Context, attrs:AttributeSet): View(context, attrs) {
     private var color = Color.BLACK
     private var canvas: Canvas? = null
     private val mPaths = ArrayList<CustomPath>()
+    private val mUndoPaths = ArrayList<CustomPath>()
 
 
     init{
         setupDrawing()
+    }
+
+    fun onClickUndo(){
+        if(mPaths.size> 0 ){
+            mUndoPaths.add(mPaths.removeAt(mPaths.size-1))
+            invalidate()
+        }
     }
 
     private fun setupDrawing(){
--- app/src/main/java/com/drawindapp/colourbook_drawandpaint/MainActivity.kt
+++ app/src/main/java/com/drawindapp/colourbook_drawandpaint/MainActivity.kt
@@ -1,8 +1,20 @@
+@file:Suppress("DEPRECATION")
+
 package com.drawindapp.colourbook_drawandpaint
 
 import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.app.Dialog
+import android.app.ProgressDialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.os.AsyncTask
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.widget.ImageButton
 import android.widget.Toast
@@ -12,7 +24,11 @@ import androidx.core.content.ContextCompat
 import androidx.core.view.get
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.dialog_brush_size.*
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
 
     private var mImageButtonCurrentPaint: ImageButton? = null
@@ -31,7 +47,50 @@ class MainActivity : AppCompatActivity() {
         ib_brush.setOnClickListener {
         showBrushSizeChooserDialog()
         }
+
+        ib_gallery.setOnClickListener{
+            if (isReadStorageAllowed())
+            {
+                val pickPhotoIntent = Intent(Intent.ACTION_PICK,
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+
+                startActivityForResult(pickPhotoIntent, GALLERY)
+            }else{
+                requestStoragePermission()
+            }
+        }
+
+        ib_undo.setOnClickListener{
+            drawing_view.onClickUndo()
+        }
+
+        ib_save.setOnClickListener {
+            if(isReadStorageAllowed()){
+                BitmapAsyncTask(getBitmapFromView(fl_drawing_view_container)).execute()
+            }else{
+                requestStoragePermission()
+            }
+        }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK){
+            if (requestCode == GALLERY){
+                try {
+                    if (data!!.data != null){
+                        iv_background.visibility = View.VISIBLE
+                        iv_background.setImageURI(data.data)
+                    }else{
+                        Toast.makeText(this,"Error Parsing the Image or its corrupted", Toast.LENGTH_SHORT).show()
+                    }
+                }catch (e : Exception){
+                    e.printStackTrace()
+                }
+            }
+        }
+    }
+
 
     private fun showBrushSizeChooserDialog(){
         val brushDialog = Dialog(this)
@@ -87,7 +146,126 @@ class MainActivity : AppCompatActivity() {
         )
     }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == STORAGE_PERMISSION_CODE){
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED ){
+                Toast.makeText(this@MainActivity,
+                    "Permission granted",
+                    Toast.LENGTH_LONG).show()
+            }else{
+                Toast.makeText(this@MainActivity,
+                    "Permission Denied",
+                    Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun isReadStorageAllowed():Boolean{
+        val result = ContextCompat.checkSelfPermission(this,Manifest.permission.READ_EXTERNAL_STORAGE)
+        return result == PackageManager.PERMISSION_GRANTED
+
+    }
+
+    private fun getBitmapFromView(view:View): Bitmap {
+        val returnedBitmap = Bitmap.createBitmap(view.width,
+            view.height ,
+            Bitmap.Config.ARGB_8888)
+
+        val canvas = Canvas(returnedBitmap)
+        val bgDrawable = view.background
+        if(bgDrawable != null){
+            bgDrawable.draw(canvas)
+        }else{
+            canvas.drawColor(Color.WHITE)
+        }
+
+        view.draw(canvas)
+
+        return returnedBitmap
+    }
+
+    @Suppress("DEPRECATION")
+    @SuppressLint("StaticFieldLeak")
+    private inner class BitmapAsyncTask(val mBitmap: Bitmap?) :
+        AsyncTask<Any, Void, String>() {
+
+        @Suppress("DEPRECATION")
+        private var mDialog: ProgressDialog? = null
+
+        override fun onPreExecute() {
+            super.onPreExecute()
+
+            showProgressDialog()
+        }
+
+        override fun doInBackground(vararg params: Any): String {
+
+            var result = ""
+
+            if (mBitmap != null) {
+
+                try {
+                    val bytes = ByteArrayOutputStream()
+                    mBitmap.compress(Bitmap.CompressFormat.PNG, 90, bytes)
+
+                    val f = File(
+                        externalCacheDir!!.absoluteFile.toString()
+                                + File.separator
+                                + "ColourBook_"
+                                + System.currentTimeMillis() / 1000 + ".jpg"
+                    )
+                    val fos = FileOutputStream(f) // Creates a file output stream to write to the file represented by the specified object.
+                    fos.write(bytes.toByteArray()) // Writes bytes from the specified byte array to this file output stream.
+                    fos.close() // Closes this file output stream and releases any system resources associated with this stream. This file output stream may no longer be used for writing bytes.
+                    result = f.absolutePath // The file absolute path is return as a result.
+                } catch (e: Exception) {
+                    result = ""
+                    e.printStackTrace()
+                }
+            }
+            return result
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            cancelProgressDialog()
+            if (!result!!.isEmpty()) {
+                Toast.makeText(
+                    this@MainActivity,
+                    "File saved successfully :$result",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Something went wrong while saving the file.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+        private fun showProgressDialog() {
+            @Suppress("DEPRECATION")
+            mDialog = ProgressDialog.show(
+                this@MainActivity,
+                "",
+                "Saving your image..."
+            )
+        }
+        private fun cancelProgressDialog() {
+            if (mDialog != null) {
+                mDialog!!.dismiss()
+                mDialog = null
+            }
+        }
+    }
+
     companion object{
         private const val STORAGE_PERMISSION_CODE = 1
+        private const val GALLERY = 2
     }
 }
